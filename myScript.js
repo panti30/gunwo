3be8ca116edb5f6f6dab1799730c4a5b63a81d95
panti30 <panti30@gmail.com>
2019-01-25 17:41:08 +0100

    Initial commit

---
+++ myScript.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head> 
+<body>
+  
+    function myFunction() {
+
+document.getElementById("demo").innerHTML = "Paragraph changed / second file";
+
+}
+
+</body>
+</html> 
